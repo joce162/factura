1684facd9aa6e90e3b33268ff12500dffcb66dd3
joce162 <jo.carvacho@duocuc.cl>
2026-06-17 23:45:33 -0400

    Delete factura directory

--- factura/src/main/java/com/mayorista/factura/dto/FacturaDTO.java
+++
@@ -1,9 +0,0 @@
-package com.mayorista.factura.dto;
-import lombok.Data;
-
-@Data
-public class FacturaDTO {
-    private Long idPedido;
-    private Long idCliente;
-    private Double montoFinal;
-}
--- factura/src/main/java/com/mayorista/factura/repository/FacturaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mayorista.factura.repository;
-import com.mayorista.factura.model.Factura;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FacturaRepository extends JpaRepository<Factura, Long> {
-}
